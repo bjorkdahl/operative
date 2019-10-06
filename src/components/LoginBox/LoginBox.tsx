138e8e701908f5a0343bc8bf701ff45f92095ba8
Adam <adam@typsnith.se>
2019-10-06 19:35:57 +0200

    wip

--- src/components/LoginBox/LoginBox.tsx
+++ src/components/LoginBox/LoginBox.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Avatar from '@material-ui/core/Avatar'
 import { Formik, Form, Field, ErrorMessage, FormikValues } from 'formik'
 import Link from '@material-ui/core/Link'
-import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import Typography from '@material-ui/core/Typography'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks'
 import styles from './LoginBox.module.scss'
 import * as Yup from 'yup'
+import Heading from './../atoms/Heading/Heading'
 
 const SIGNIN_USER = gql`
   mutation SignInUser($username: String!, $password: String!) {
@@ -45,44 +44,39 @@ const LoginBox: React.FunctionComponent = () => {
   }
 
   return (
-    <Grid container component="main">
-      <Grid item xs={false} sm={4} md={7} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div>
-          <Avatar>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={SignupSchema}
-            onSubmit={onSubmit}
-          >
-            {({ isSubmitting }): any => (
-              <Form>
-                <Field type="email" name="email" />
-                <ErrorMessage name="email" component="div" />
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" component="div" />
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-              </Form>
-            )}
-          </Formik>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#">Forgot password?</Link>
-            </Grid>
-            <Grid item>
-              <Link href="#">{"Don't have an account? Sign Up"}</Link>
-            </Grid>
-          </Grid>
-        </div>
+    <div className={styles.container}>
+      <Heading large>
+        <Avatar>
+          <LockOutlinedIcon />
+        </Avatar>
+        Sign in
+      </Heading>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={SignupSchema}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting }): any => (
+          <Form>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+      <Grid container>
+        <Grid item xs>
+          <Link href="#">Forgot password?</Link>
+        </Grid>
+        <Grid item>
+          <Link href="#">{"Don't have an account? Sign Up"}</Link>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   )
 }
 
