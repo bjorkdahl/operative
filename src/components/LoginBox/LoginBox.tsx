83a1919839c2cd5e83a17cf106838cbe8ea8d362
Adam <adam@typsnith.se>
2019-10-07 21:42:46 +0200

    wip

--- src/components/LoginBox/LoginBox.tsx
+++ src/components/LoginBox/LoginBox.tsx
@@ -3,16 +3,48 @@ import Avatar from '@material-ui/core/Avatar'
 import { Formik, Form, Field, ErrorMessage, FormikValues } from 'formik'
 import Link from '@material-ui/core/Link'
 import Grid from '@material-ui/core/Grid'
+import { TextField } from 'formik-material-ui'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks'
 import styles from './LoginBox.module.scss'
 import * as Yup from 'yup'
 import Heading from './../atoms/Heading/Heading'
+import strings from 'strings'
+import Text from 'components/atoms/Text'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: '#9FEDD7',
+    width: '60px',
+    height: '60px',
+  },
+  icon: {
+    width: '35px',
+    height: '35px',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    width: '100%',
+    height: '300px',
+  },
+  field: {
+    padding: '5px',
+    margin: '15px 5px',
+    color: '#9FEDD7',
+  },
+})
 
 const SIGNIN_USER = gql`
   mutation SignInUser($username: String!, $password: String!) {
-    signIn(data: { username: $username, password: $password })
+    signIn(data: { username: $username, password: $password }) {
+      user {
+        token
+      }
+    }
   }
 `
 
@@ -27,6 +59,7 @@ const SignupSchema = Yup.object().shape({
 })
 
 const LoginBox: React.FunctionComponent = () => {
+  const classes = useStyles()
   const [signInUser] = useMutation(SIGNIN_USER)
   const initialValues = { email: '', password: '' }
   const onSubmit = async (
@@ -44,39 +77,60 @@ const LoginBox: React.FunctionComponent = () => {
   }
 
   return (
-    <div className={styles.container}>
-      <Heading large>
-        <Avatar>
-          <LockOutlinedIcon />
-        </Avatar>
-        Sign in
-      </Heading>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={SignupSchema}
-        onSubmit={onSubmit}
-      >
-        {({ isSubmitting }): any => (
-          <Form>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-      <Grid container>
-        <Grid item xs>
-          <Link href="#">Forgot password?</Link>
-        </Grid>
-        <Grid item>
-          <Link href="#">{"Don't have an account? Sign Up"}</Link>
+    <Grid container className={styles.background}>
+      <Grid item xs={12} sm={6} md={4} lg={3} className={styles.box}>
+        <div className={styles.header}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon className={classes.icon} />
+          </Avatar>
+          <Heading>Sign in</Heading>
+        </div>
+
+        <Formik
+          initialValues={initialValues}
+          validationSchema={SignupSchema}
+          onSubmit={onSubmit}
+        >
+          {({ isSubmitting }): any => (
+            <Form className={classes.form}>
+              <Field
+                className={classes.field}
+                component={TextField}
+                label={strings.get('USERNAME')}
+                name="email"
+                type="email"
+              />
+              <Field
+                className={classes.field}
+                component={TextField}
+                label={strings.get('PASSWORD')}
+                name="password"
+                type="password"
+              />
+              <button
+                className={styles.button}
+                type="submit"
+                disabled={isSubmitting}
+              >
+                <Text bold>{strings.get('SUBMIT')}</Text>
+              </button>
+            </Form>
+          )}
+        </Formik>
+        <Grid container className={classes.field}>
+          <Grid item xs>
+            <Link href="#">
+              <Text>{strings.get('FORGOT_PASSWORD')}</Text>
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="#">
+              <Text>{strings.get('SIGN_UP')}</Text>
+            </Link>
+          </Grid>
         </Grid>
       </Grid>
-    </div>
+    </Grid>
   )
 }
 
--- src/strings/strings.tsx
+++ src/strings/strings.tsx
@@ -1,6 +1,11 @@
 import get from 'lodash/get'
 
 const sv = {
+  SUBMIT: 'Submit',
+  USERNAME: 'User name',
+  PASSWORD: 'Password',
+  FORGOT_PASSWORD: 'Forgot your password?',
+  SIGN_UP: 'Sign up!',
   TAGLINE: 'Datadriven hälsoanalys',
   TAGLINE_DESC:
     'Samla all din information från Runkeeper, Strava, Samsung Health och Apple Health på ett och samma ställe',
